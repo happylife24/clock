b25be77115423bb84c3d0079b1fde7c9c2a35dcc
2021sky <124482174+2021sky@users.noreply.github.com>
2023-11-04 05:39:00 -0400

    .

---
+++ clock.js
@@ -0,0 +1,11 @@
+// 배경색
+function changeBackgroundColor() {
+    document.body.style.backgroundColor = "green";
+} 
+changeBackgroundColor();
+
+
+// 배경사진 사이즈
+const image = document.getElementById("newImage");
+image.style.width = "500px";
+image.style.height = "500px";
