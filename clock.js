a998b1809786ac1ea71e8f05eeffa1af2f775740
58sky <58sky2021@gmail.com>
2023-11-05 01:06:06 -0400

    .

--- clock.js
+++ clock.js
@@ -5,7 +5,14 @@ function changeBackgroundColor() {
 changeBackgroundColor();
 
 
-var image = document.getElementById("newPic");
-var newWidth = 200;
-var newHeight = 300;
+//사이즈 변경
+function resizeImage() {
+    var image = document.getElementById("myImage");
+    image.width = 600;
+    image.height = 400;
+}
+//변경된 이미지 불러오기
+window.onload = resizeImage;
+
+
 
