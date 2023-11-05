06cbcd01ab3bc2f37a8521e4c1fa8e3a0ddb2b3d
58sky <58sky2021@gmail.com>
2023-11-05 01:12:53 -0500

    .

--- clock.js
+++ clock.js
@@ -20,7 +20,7 @@ var image = document.getElementById("myImage");
 var currentPosition = 0; //현재위치
 //오른쪽으로 이동 함수
 function moveImageRight() {
-    currentPosition += 10; //이동거리조절
+    currentPosition += 50; //이동거리조절
     image.style.left = currentPosition + px;
 }
 //호출시 1초마다 오른쪽 이동
