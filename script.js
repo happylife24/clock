ac78b70fab7430c81b0a35b088121ea4de03ac2b
58sky <58sky2021@gmail.com>
2023-11-20 11:45:51 -0500

    .

--- script.js
+++ script.js
@@ -8,8 +8,8 @@ changeBackgroundColor();
 //resize image
 function resizeImage() {
     var image = document.getElementById("myImage");
-    image.width = 600;
-    image.height = 400;
+    image.width = 900;
+    image.height = 600;
 }
 //call modified image 
 window.onload = resizeImage;
@@ -17,9 +17,26 @@ window.onload = resizeImage;
 
 //style image
 var myFunction =
-    document.getElementById("myImage").style.left = "310px";
-    document.getElementById("myImage").style.top = "100px";
+    document.getElementById("myImage").style.left = "170px";
+    document.getElementById("myImage").style.top = "15px";
     document.getElementById("myImage").style.borderRadius = "50px";
+
+
+//time
+function beginTime() {
+    const now = new Date();
+    let h = now.getHours();
+    let m = now.getMinutes();
+    let s = now.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('txt').innerHTML = h + ":" + m + ":" + s;
+    setTimeout(beginTime, 1000);
+}
+function checkTime(i) {
+    if (i < 10) {i = "0" + i};
+    return i;
+}
 
 
 
