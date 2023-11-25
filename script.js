89153782ccaff61335736ec554b8805c162bd39d
58sky <58sky2021@gmail.com>
2023-11-24 20:31:59 -0500

    .

--- script.js
+++ script.js
@@ -25,8 +25,12 @@ function checkTime(i) {
 
 //clock moves right
     document.getElementById("txtClock").style.backgroundColor = "yellow";
-    document.getElementById("txtClock").style.right = "280px";
+    document.getElementById("txtClock").style.right = "300px";
     document.getElementById("txtClock").style.top = "80px";
+    document.getElementById("txtClock").style.borderRadius = "70px";
+    document.getElementById("txtClock").style.display = "flex";
+    document.getElementById("txtClock").style.justifycontent = "center";
+    document.getElementById("txtClock").style.alignItems = "center";
 
 //clock-img  &  img-position
 
