d911418d3e51e07741d61300c752cbc56315b1fb
58sky <58sky2021@gmail.com>
2023-11-25 23:22:47 -0500

    .

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //background color
 function changeBackgroundColor() {
-    document.body.style.backgroundColor = "black";
+    document.body.style.backgroundColor = "brown";
 } 
 changeBackgroundColor();
 
@@ -20,16 +20,19 @@ window.onload = showTxtClock;
 
 
 //clock move & style
-    document.getElementById("txtClock").style.backgroundColor = "black";
+    document.getElementById("txtClock").style.backgroundColor = "white";
     document.getElementById("txtClock").style.right = "60px";
     document.getElementById("txtClock").style.top = "30px";
     document.getElementById("txtClock").style.borderRadius = "200px";
-    document.getElementById("txtClock").style.border = "80px double yellow";
+    document.getElementById("txtClock").style.border = "80px double black";
     document.getElementById("txtClock").style.fontSize = "100px";
-    document.getElementById("txtClock").style.color = "white";
+    document.getElementById("txtClock").style.color = "black";
     document.getElementById("txtClock").style.display = "flex";
     document.getElementById("txtClock").style.justifycontent = "center";
     document.getElementById("txtClock").style.alignItems = "center";
+
+
+//
 
 
 
