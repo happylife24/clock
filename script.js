6b9cfba89e292368ee730eaff0039d604ea1d928
58sky <58sky2021@gmail.com>
2023-11-28 23:58:32 -0500

    .

--- script.js
+++ script.js
@@ -24,18 +24,24 @@ window.onload = showTxtClock;
 
 
 //clock move & style
+function styleClock() {
     document.getElementById("txtClock").style.backgroundColor = "black";
-    document.getElementById("txtClock").style.right = "60px";
+    document.getElementById("txtClock").style.right = "100px";
     document.getElementById("txtClock").style.top = "30px";
     document.getElementById("txtClock").style.width = "1000px";
-    document.getElementById("txtClock").style.height = "400px";
+    document.getElementById("txtClock").style.height = "300px";
     document.getElementById("txtClock").style.borderRadius = "200px";
-    document.getElementById("txtClock").style.border = "80px double yellow";
+    document.getElementById("txtClock").style.border = "40px double yellow";
     document.getElementById("txtClock").style.fontSize = "100px";
     document.getElementById("txtClock").style.color = "white";
-    document.getElementById("txtClock").style.textAlign = "center";
-   
+}
+styleClock();
     
+
+//move only text
+var txtClock = document.getElementById("txtClock");
+txtClock.style.paddingTop = "180px";
+
 
 
 
