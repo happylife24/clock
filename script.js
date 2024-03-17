aba3d069cf4c35901f107997386b04d6a9e7f244
58sky <58sky2021@gmail.com>
2024-03-16 21:49:26 -0400

    .

--- script.js
+++ script.js
@@ -58,17 +58,17 @@ function styleClock() {
     txtClock.style.backgroundImage= "radial-gradient(circle at 80% 70%, red 1%, transparent 10%), " + "radial-gradient(circle at 90% 20%, yellow 1%, transparent 10%), " + "radial-gradient(circle at 20% 20%, green 1%, transparent 20%), " + "radial-gradient(circle at 50% 70%, blue 1%, transparent 30%), " + "radial-gradient(circle at 10% 80%, pink 0.1%, transparent 15%)";
     txtClock.style.right = "0";
     txtClock.style.left = "0";
+    txtClock.style.top = "100px";
+    txtClock.style.paddingTop = "100px";
     txtClock.style.margin = "0 auto";
-    txtClock.style.top = "13%";
-    txtClock.style.width = "70%";
-    txtClock.style.height = "60%";
+    txtClock.style.width = "450px";
+    txtClock.style.height = "260px";
     txtClock.style.borderRadius = "30px";
     txtClock.style.fontSize = "10vw";
     txtClock.style.fontFamily = "Arial, sans-serif";
     txtClock.style.textAlign = "center";
     txtClock.style.backgroundColor = "purple";
     txtClock.style.color = "white";
-    txtClock.style.paddingTop = "3%";
 }
 styleClock();
 
